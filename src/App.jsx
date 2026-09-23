f1e4b6b0fc1c964bcfab1ba2479fc339ea2fb4f1
Harshita <harshitaworld9965@gmail.com>
2026-09-23 15:28:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function App(){
+  return (
+    <main className="hero">
+      <header className="brand">KAELIX</header>
+
+      <section className="hero-content">
+        <p className="eyebrow">CREATIVE TECHNOLOGY STUDIO</p>
+
+        <h1>We build digital things
+          <br />
+          at the intersection of
+          <br/>
+          design and technology.
+        </h1>
+
+        <p className="description">
+          Digital products, interfaces and AI-powered experiments
+          built with curiosity and intention.
+        </p>
+
+        <button>Explore the work</button>
+      </section>
+    </main>
+  );
+}
+export default App;
